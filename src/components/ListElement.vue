f4b7db49487f0d8fa7cc52c5518045c7d0b4f519
Tobias <tobias.joschko@gmx.de>
2024-01-30 16:21:23 +0100

    Zeige unterscheidliche Vue Konzepte

---
+++ src/components/ListElement.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+// give each todo a unique id
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML', done: false },
+  { id: id++, text: 'Learn JavaScript', done: false },
+  { id: id++, text: 'Learn Vue', done: false }
+])
+const hideCompleted = ref(false);
+
+const filteredTodos = computed(() => {
+  if (hideCompleted.value) {
+    return todos.value.filter(todo => !todo.done)
+  } else {
+    return todos.value
+  }
+})
+function addTodo() {
+  todos.value.push({
+    id: id++,
+    text: newTodo.value
+  })
+  newTodo.value = ''
+}
+function hideCompletedItems() {
+  hideCompleted.value = !hideCompleted.value;
+}
+
+function removeTodo(todo) {
+  // ...
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>    
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompletedItems">
+  {{ hideCompleted ? "Hide Completed Tasks" : "Show Completed Tasks" }}
+  </button>
+</template>
--- src/views/VorlesungsView.vue
+++ src/views/VorlesungsView.vue
@@ -1,6 +1,10 @@
 <script setup>
 import DynamicComp from '@/components/DynamicComp.vue';
 import AttributeBinding from '@/components/AttributeBinding.vue';
+import EventListener from '@/components/EventListener.vue';
+import FormBindung from '@/components/FormBindung.vue';
+import VIF from '@/components/VIF.vue';
+import ListElement from '@/components/ListElement.vue';
 </script>
 <template>
   <div class="about">
@@ -10,6 +14,18 @@ import AttributeBinding from '@/components/AttributeBinding.vue';
 
     <h2>Attribute Binding:</h2>
     <AttributeBinding />
+
+    <h2>Event Listener</h2>
+    <EventListener />
+
+    <h2>Formbinding</h2>
+    <FormBindung />
+
+    <h2>V-IF</h2>
+    <VIF />
+
+    <h2>Listen in Vue</h2>
+    <ListElement />
   </div>
 </template>
 
